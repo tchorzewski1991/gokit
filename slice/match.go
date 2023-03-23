701be97f95015610d018ab0b7250da1f6e893050
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-03-23 17:51:59 +0100

    Rename matcher -> match + update docs

--- slice/match.go
+++ slice/match.go
@@ -1,5 +1,5 @@
-package collection
+package slice
 
 // Match provides unified signature used to verify whether given condition has been met.
-// Dig into collection/matchers to take advantage of predefined matchers.
+// Dig into collection/matchers to take advantage of predefined matching funcs.
 type Match[T comparable] func(elem T) bool
